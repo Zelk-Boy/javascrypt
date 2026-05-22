e59021ca91059e95cd58277c457d83b9ab16da83
Zelk-Boy <TechZelk@proton.me>
2026-05-22 14:47:46 -0600

    Fix reassignment of variable 'pi' in javascript.js

--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ console.log(42+6+46+112+5+2.22);
     console.log(age);
     let pi = 3.14;
 pi = 10;
-//pi = 4
+pi = 4
 console.log(pi); // What will be output?
 console.log((4 + 6 + 9) / 77);
 let a = 10
