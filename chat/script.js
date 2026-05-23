92a2c1d6dce844e7522f9bfc4c7d8c3229774e8e
Zelk Zephner <TechZelk@proton.me>
2026-05-23 17:52:04 -0600

    Update script.js

--- chat/script.js
+++ chat/script.js
@@ -1,1 +1,27 @@
-console.log("JS FILE IS LOADING");
+let button = document.getElementById("chatButton");
+let input = document.getElementById("userInput");
+let chatBox = document.getElementById("chatBox");
+
+button.addEventListener("click", function () {
+
+  let userText = input.value;
+
+  chatBox.innerHTML += "<p><b>You:</b> " + userText + "</p>";
+
+  let response = "";
+
+  if (userText === "hello") {
+    response = "Hello. I am Zephner Systems AI.";
+  } 
+  else if (userText === "how are you") {
+    response = "Systems operational.";
+  } 
+  else {
+    response = "Command not recognized.";
+  }
+
+  chatBox.innerHTML += "<p><b>AI:</b> " + response + "</p>";
+
+  input.value = "";
+
+});
