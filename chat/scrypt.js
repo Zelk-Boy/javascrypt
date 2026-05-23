03d98df3762743641bc9811b2e94859fab71cfb8
Zelk Zephner <TechZelk@proton.me>
2026-05-23 17:33:50 -0600

    Update scrypt.js

--- chat/scrypt.js
+++ chat/scrypt.js
@@ -1,1 +1,9 @@
-console.log(ZelkChat is Online!)
+let button = document.getElementById("chatButton");
+
+let message = document.getElementById("message");
+
+button.addEventListener("click", function () {
+
+  message.textContent = "Hello. I am Zephner Systems AI.";
+
+});
